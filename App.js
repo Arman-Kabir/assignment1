b8ef282f81eb8d1b6f4f79c355dcd231e77b92d3
Arman-Kabir <armankabir8@gmail.com>
2024-03-07 13:00:16 +0600

    styles added intro and text

--- App.js
+++ App.js
@@ -5,45 +5,47 @@ export default function App() {
   return (
     <View style={styles.container}>
       <ScrollView>
-        <View>
-          <Image style={styles.logo} source={require('./assets/arman.png')}></Image>
-          <View>
-            <Text>The Octocat</Text>
-            <Text>@octocat</Text>
-            <Text>Joined 25 Jan 2011</Text>
+        <View style={styles.card}>
+          <View style={styles.intro}>
+            <Image style={styles.logo} source={require('./assets/arman.png')}></Image>
+            <View>
+              <Text style={styles.title}>The Octocat</Text>
+              <Text  style={styles.sub_title}>@octocat</Text>
+              <Text style={styles.common_text_style}>Joined 25 Jan 2011</Text>
+            </View>
           </View>
-        </View>
-        <Text> I am learning React Native from Saad Sir. The instructor explains core concepts in a easy way and i can understand the lessons very easily. This is my first project in react native and i am enjoying the journey.    </Text>
-        <View>
+          <Text style={styles.common_text_style}>I am learning React Native from Saad Sir. The instructor sir explains core concepts in a easy way </Text>
           <View>
-            <Text>Repos</Text>
-            <Text>8</Text>
-          </View>
-          <View>
-            <Text>Followers</Text>
-            <Text>3938</Text>
-          </View>
-          <View>
-            <Text>Following</Text>
-            <Text>9</Text>
-          </View>
-        </View>
-        <View>
-          <View>
-            <Image source={require('./assets/map.png')}></Image>
-            <Text>San Francisco</Text>
+            <View>
+              <Text>Repos</Text>
+              <Text>8</Text>
+            </View>
+            <View>
+              <Text>Followers</Text>
+              <Text>3938</Text>
+            </View>
+            <View>
+              <Text>Following</Text>
+              <Text>9</Text>
+            </View>
           </View>
           <View>
-            <Image source={require('./assets/links.png')}></Image>
-            <Text>https://github.blog</Text>
-          </View>
-          <View>
-            <Image source={require('./assets/twitter.png')}></Image>
-            <Text>Not available</Text>
-          </View>
-          <View>
-            <Image source={require('./assets/building.png')}></Image>
-            <Text>@github</Text>
+            <View>
+              <Image source={require('./assets/map.png')}></Image>
+              <Text>San Francisco</Text>
+            </View>
+            <View>
+              <Image source={require('./assets/links.png')}></Image>
+              <Text>https://github.blog</Text>
+            </View>
+            <View>
+              <Image source={require('./assets/twitter.png')}></Image>
+              <Text>Not available</Text>
+            </View>
+            <View>
+              <Image source={require('./assets/building.png')}></Image>
+              <Text>@github</Text>
+            </View>
           </View>
         </View>
       </ScrollView>
@@ -54,11 +56,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#f4f4f4",
-    paddingTop: 50
+  },
+  common_text_style:{
+    color:"#7c7c7d",
+    fontSize:16,
+    lineHeight:25,
+  },
+  card: {
+    backgroundColor:"#fff",   
+    marginHorizontal: 30,
+    marginVertical: 50,
+    borderRadius:20,
+    paddingVertical:40,
+    paddingHorizontal:20
+  },
+  intro:{
+    flexDirection:"row",
+    alignItems:'flex-end',
+    marginBottom:30
   },
   logo: {
-    width: 60,
-    height: 60,
-    borderRadius: 50
-  }
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+    marginRight:20
+  },
+  title:{
+    fontSize:24,
+    fontWeight:"bold"
+  },
+  sub_title:{
+    color:"#4d61e8",
+    fontSize:16,
+    fontWeight:"bold",
+    marginBottom:10
+  },
+  // paragraph
+  
 });
